5ced4841c9f840dd5557ad6acc777415eceea454
justletmecreatetheaccount <vladdoniga11@gmail.com>
2025-03-20 14:45:56 +0100

    moved caisse savon

---
+++ Savon/symbolicR/mbs_cons_hJ_MON_LIV.py
@@ -0,0 +1,38 @@
+#
+#	MBsysTran - Release 8.1
+#
+#	Copyright 
+#	Universite catholique de Louvain (UCLouvain) 
+#	Mechatronic, Electrical Energy, and Dynamic systems (MEED Division) 
+#	2, Place du Levant
+#	1348 Louvain-la-Neuve 
+#	Belgium 
+#
+#	http://www.robotran.be 
+#
+#	==> Generation Date: Tue Mar 18 12:43:11 2025
+#	==> using automatic loading with extension .mbs 
+#
+#	==> Project name: MON_LIV
+#
+#	==> Number of joints: 12
+#
+#	==> Function: F8 - Constraints and Constraints Jacobian(h, J)
+#
+#	==> Git hash: a9259031f25de7c9f86dbac08464ef31e84ae3b1
+#
+#	==> Input XML
+#
+
+from math import sin, cos
+
+def cons_hJ(h, Jac, s):
+    q = s.q
+
+# Number of continuation lines = 0
+
+    print("ERROR : Your symbolic files seem obsolete, i.e. not up-to-date with your MBsysPad model. ")
+    print("        Please regenerate your symbolic files (MBsysPad->Tools->Generate Symbolic Files). Exiting. ")
+    print("        Error raised in mbs_cons_hJ.")
+    s.flag_stop = 1
+
---
+++ Savon/userfctR/user_DrivenJoints.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""Module for the definition of driven joints."""
+# Author: Robotran Team
+# (c) Universite catholique de Louvain, 2020
+
+
+def user_DrivenJoints(mbs_data, tsim):
+    """Set the values of the driven joints directly in the MbsData structure.
+
+    The position, velocity and acceleration of the driven joints must be set in
+    the attributes mbs_data.q, mbs_data.qd and mbs_data.qdd .
+
+    Parameters
+    ----------
+    mbs_data : MBsysPy.MbsData
+        The multibody system associated to this computation.
+    tsim : float
+        The current time of the simulation.
+
+    Returns
+    -------
+    None
+    """
+    id_j = mbs_data.joint_id["Joint_8"]
+
+    mbs_data.qdd[id_j] = 0
+    mbs_data.qd[id_j]  = 0.01
+    mbs_data.q[id_j]   = tsim * 0.01
+
+    id_i = mbs_data.joint_id["Joint_7"]
+
+    mbs_data.qdd[id_i] = 0
+    mbs_data.qd[id_i]  = 0.01
+    mbs_data.q[id_i]   = tsim * 0.01
+
+    # Example: joint 5 under constant acceleration with non-zero initial
+    #          coordinate (mbs_data.q0) and velocity (mbs_data.qd0).
+    # mbs_data.qdd[5] = 2
+    # mbs_data.qd[5]  = mbs_data.qd0[5] + mbs_data.qdd[5]*tsim
+    # mbs_data.q[5]   = mbs_data.q0[5]  + mbs_data.qd0[5]*tsim + 0.5 * mbs_data.qdd[5]*tsim*tsim
+
+    return
