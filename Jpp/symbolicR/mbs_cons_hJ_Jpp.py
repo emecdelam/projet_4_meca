d1b572201e46dde5bf248efe746c28ebde45c580
justletmecreatetheaccount <vladdoniga11@gmail.com>
2025-03-24 20:42:50 +0100

    adding more distance between fusse and cremaillere

--- Jpp/symbolicR/mbs_cons_hJ_Jpp.py
+++ Jpp/symbolicR/mbs_cons_hJ_Jpp.py
@@ -10,7 +10,7 @@
 #
 #	http://www.robotran.be 
 #
-#	==> Generation Date: Mon Mar 24 20:37:20 2025
+#	==> Generation Date: Mon Mar 24 20:40:33 2025
 #	==> using automatic loading with extension .mbs 
 #
 #	==> Project name: Jpp
