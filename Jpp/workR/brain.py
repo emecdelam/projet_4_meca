a285f73106bdeb6bb2beeb859088b3d1e2d58b1c
emecdelam <kiwi.thefruit0000@gmail.com>
2025-03-28 23:51:45 +0100

    tuning brain.py

--- Jpp/workR/brain.py
+++ Jpp/workR/brain.py
@@ -40,20 +40,24 @@ par.append_parameter(params_path ,parameters)
 mbs_data = api.loader(mbs_path)
 
 # Configuration
-names = ["Analyse de la position pour plusieur Kext",
-         "Analyse de la position pour plusieur Dext", 
-         "Analyse de la position pour plusieur poids passager"]
+names = [#"Analyse de la position pour plusieurs constantes de raideurs des pneux (K_pneu)",
+         #"Analyse de la position pour plusieurs constantes d'amortissement des pneux (D_pneu)", 
+         #"Analyse de la position pour plusieurs constantes de raideurs des suspensions",
+         "Analyse de la position pour plusieurs constantes d'amortissement des suspensions"]
 utils.log(f"Configuring names : {names}", utils.Level.INFO)
 
-labels = [('Temps [s]', 'Hauteur du corps [m]') for i in range(2)]
+labels = [('Temps [s]', 'Hauteur du bus [m]') for i in range(len(names))]
 utils.log(f"Configuring labels : {labels}", utils.Level.INFO)
 
-params = [[{'Kext': 3000.0}, {'Kext': 1000.0},{'Kext': 500.0}],
-          [{'Kext':2000.0,'Dext': 300.0}, {'Dext': 100.0}, {'Dext' : 150.0}],
-          [{'Fz':2000000, 'Fz':3000000,'Fz':500000}]]
+params = [#[{'Kext': 1600000}, {'Kext': 2000000},{'Kext': 1200000}],
+          #[{'Dext': 150.0}, {'Dext': 200.0}, {'Dext' : 100.0}],
+          #[{'Ks':50000, 'Ksd':600000}, {'Ks':70000, 'Ksd': 800000}, {'Ks':30000, 'Ksd': 400000}],
+          [{'Dd':120, 'Dsd':80000},{'Dd':170, 'Dsd':100000},{'Dd':70, 'Dsd':60000}]]
 utils.log(f"Configuring params : {params}", utils.Level.INFO)
 
-legends = [[f'Position du chariot avec {dict_str(i)}' for i in j] for j in params]
+def repl(s):
+    return s.replace("Kext","K_pneu").replace("Dext","D_pneu").replace("Ksd","K_avant").replace("Dsd","D_avant").replace("L0sd","L0_avant").replace("Ks","K_arrière").replace("Dd","D_arrière").replace("L0s","L0_arrière")
+legends = [[f'{repl(dict_str(i))}' for i in j] for j in params]
 utils.log(f"Configuring legends : {legends}", utils.Level.INFO)
 
 ylims = [None for i in labels]
@@ -78,7 +82,7 @@ with Progress() as progress:
                     utils.log("Starting new dynamic simulation", utils.Level.INFO)
                     par.append_parameters(params_path,[params[plot][curve]])
                     results, nt0, ntf = api.dir_dyn(mbs_data)
-                    axis.plot(results.q[:, 0], results.q[:, 4], label=legends[plot][curve])
+                    axis.plot(results.q[:, 0], results.q[:, 1], label=legends[plot][curve])
                     progress.update(taks_dyn, advance=1)
                     t0 = min(t0, nt0)
                     tf = max(tf, ntf)
@@ -86,6 +90,7 @@ with Progress() as progress:
                     utils.log(f"Failed to get the curve due to {e}", utils.Level.ERROR)
             api.plot_finalizer(axis, (t0, tf), ylims[plot])
             plt.savefig(names[plot])
+            par.append_parameter(params_path ,parameters)
             progress.update(task_plot, advance=1)
         except Exception as e:
             utils.log(f"Failed to plot due to {e}", utils.Level.ERROR)
